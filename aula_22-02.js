cd46e681cdb8a8c43a3e53efd41a5ba9ebda6b01
Annegabrielebassi <a.bassi@estudante.ifro.edu.br>
2024-02-22 11:26:28 -0400

    first commit

---
+++ aula_22-02.js
@@ -0,0 +1,21 @@
+// qst1
+var nome='Anne Gabriele';
+var idade=15;
+console.log('Olá,meu nome é',nome,'e eu tenho ',idade,'anos')
+
+//qst2
+console.log('problema'.toUpperCase());
+console.log('LALALA'.toLocaleLowerCase());
+
+//qst3
+var num='24';
+console.log(typeof num);
+
+//qst4
+var altura=1.58;
+var peso=52.00;
+console.log(peso/altura**2);
+
+//qst5
+
+
